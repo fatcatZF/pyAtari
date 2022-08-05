47a2c8fe4c0373de4046a4be94ac70ea132dfbfa
ZFang <z.fang0512@gmail.com>
2022-08-05 13:43:47 +0200

    frame processor

--- utils.py
+++ utils.py
@@ -1,14 +1,26 @@
 import gym
 import numpy as np
 import cv2
+import matplotlib.pyplot as plt
+import torch
 
-def process_frame(frame):
-    """crop the frame image"""
+def frameProcessor(frame, target_height=84, target_width=84, is_plot=False):
+    """
+    process a single frame
+    :param frame: the frame to be processed
+    :param target_height: the target height
+    :param target_width: the target width
+    :param is_plot: whether plot the processed frame
+    :return:
+    """
     frame = frame[30:-12, 5:-4]
     frame = np.average(frame, axis=2)
-    frame = cv2.resize(frame, (84,84), interpolation=cv2.INTER_NEARST)
+    frame = cv2.resize(frame, (target_height, target_width),
+                       interpolation=cv2.INTER_NEAREST)
     frame = np.array(frame, dtype=np.uint8)
-    return frame
+    if is_plot:
+        plt.imshow(frame)
+    return torch.from_numpy(frame)
 
 
 
